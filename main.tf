71b906a72d6b6cb007e17e9e05bc66dfd332fd05
jadalaramani <jadalaramani@gmail.com>
2025-06-20 15:03:30 +0530

    update commit

--- main.tf
+++ main.tf
@@ -6,15 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "cluster1-terraform-eks-state-s3-bucket1"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-eks-state-locks"
-    encrypt        = true
-  }
 }
-
 provider "aws" {
   region = var.region
 }
